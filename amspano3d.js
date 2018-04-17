00df9c0b1d6e8076f0ac2d520a0b4125a04e1be8
Sihang Qiu <qiusihang@gmail.com>
2018-04-17 18:23:54 +0200

    add navigator to 3D viewer

--- amspano3d.js
+++ amspano3d.js
@@ -1,3 +1,8 @@
+/**
+ * @author sihang / http://github.com/qiusihang
+ * dependencies: jquery, three.js (three.min.js,OrbitControls.js,BlendShader.js,CopyShader.js,VignetteShader.js,EffectComposer.js,RenderPass.js,ShaderPass.js,SavePass.js)
+ */
+
 var AMSPano = {
 
     createNew: function(panorama, init_params = {pos:{lat: 52.376937, lng: 4.901927},zoom:1.0,pov:{heading:90,pitch:0}} )
@@ -5,16 +10,22 @@ var AMSPano = {
 
         var proxy_head = "https://calm-hamlet-41397.herokuapp.com/geturl.php?";
         var proxy_img_head = "https://calm-hamlet-41397.herokuapp.com/getimg.php?";
-        var request_head = "https://api.data.amsterdam.nl/panorama/opnamelocatie/";
-        var heading = 0; // it's the default heading of panorama, not current heading
+        var request_head = "https://api.data.amsterdam.nl/panorama/opnamelocatie/";;
         var zoom = init_params.zoom;
         var curPos = init_params.pos;
         var pov = init_params.pov;
+        var heading = 0; // it's the default heading of panorama, not current heading
+        var timestamp = "none";
         var markers = [];
         var adjacents = [];
+        var destination = []; // destinations of different directions
         var imgurl_small = "";
         var imgurl_medium = "";
         var imgurl_full = "";
+        var root = document.createElement("div");
+        var threeDview = document.createElement("div");
+        var information_panel = document.createElement("div");
+        var information_box = document.createElement("div");
         var zoominout = document.createElement("div");
         var zoomin = document.createElement("input");
         var zoomout = document.createElement("input");
@@ -25,7 +36,6 @@ var AMSPano = {
 
         //3D
         var first_draw = true;
-        var threeDview = document.createElement("div");
         var renderer;
         var camera;
         var scene;
@@ -34,6 +44,8 @@ var AMSPano = {
         var effectBlend;
         var ambientLight, spotLight;
         var mesh,loader=new THREE.TextureLoader();
+        var raycaster = new THREE.Raycaster();
+        var mouse = new THREE.Vector2();
 
         function initRender() {
             renderer = new THREE.WebGLRenderer({antialias: true});
@@ -121,7 +133,7 @@ var AMSPano = {
         }
 
         var transition_time = 0;
-        function transition()
+        function transition() // transition animation of panorama changing
         {
             if ( transition_time > 0 ) {
                 transition_time ++;
@@ -133,6 +145,31 @@ var AMSPano = {
                     camera.updateProjectionMatrix();
                 }
             }else transition_time = 0;
+        }
+
+        var navigator_plane,navigator_pointer;
+        var navigator_geometry_yes, navigator_geometry_no;
+        function initNavigator() // draw naviagtor on ground
+        {
+            var plane_geometry = new THREE.BoxBufferGeometry( 1000,0.1,1000 );  // ground
+            var plane_material = new THREE.MeshBasicMaterial({color:"white",opacity: 0});
+            plane_material.transparent = true;
+            navigator_plane = new THREE.Mesh( plane_geometry,plane_material );
+            navigator_plane.position.y = -20;
+            scene.add( navigator_plane );
+
+            navigator_geometry_yes = new THREE.CylinderBufferGeometry(10,10,0.1,30);
+            navigator_geometry_no = new THREE.Geometry();
+            var geometry_no1 = new THREE.BoxGeometry(20,0.1,5);var box1 = new THREE.Mesh(geometry_no1);
+            var geometry_no2 = new THREE.BoxGeometry(5,0.1,20);var box2 = new THREE.Mesh(geometry_no2);
+            box1.updateMatrix(); navigator_geometry_no.merge(box1.geometry, box1.matrix);
+            box2.updateMatrix(); navigator_geometry_no.merge(box2.geometry, box2.matrix);
+            var navigator_material = new THREE.MeshBasicMaterial({color:"red",opacity: 0.3});
+            navigator_material.transparent = true;
+            navigator_pointer = new THREE.Mesh( navigator_geometry_no, navigator_material );
+            navigator_pointer.rotateY(Math.PI/4.0);
+            navigator_pointer.position.y = -20;
+            scene.add( navigator_pointer );
         }
 
         function render() {
@@ -157,9 +194,12 @@ var AMSPano = {
             initComposer();
             initModel();
             initControls();
+            initNavigator();
             animate();
             window.onresize = onWindowResize;
         }
+        // End of 3D part
+
 
         panorama.set = function(pano_id="")
         {
@@ -181,12 +221,33 @@ var AMSPano = {
                     imgurl_full = proxy_img_head + request["image_sets"]["equirectangular"]["full"];
                     heading = request["heading"];
                     adjacents = request["adjacent"];
+                    timestamp = request["timestamp"];
+                    for ( var d = 0 ; d < 36 ; d ++ ) // find adjacents
+                    {
+                        var m = 0, index=-1;
+                        var h = d * 10;
+                        for ( var i = 0 ; i < adjacents.length ; i ++ )
+                        {
+                            var dir = (heading+parseInt(adjacents[i]["direction"])+180)%360;
+                            var dis = parseInt(adjacents[i]["distance"]);
+                            if ( dis > 5 && Math.abs(dir-h) < 20 && m < parseInt(adjacents[i]["year"]) )
+                            {
+                                m = parseInt(adjacents[i]["year"]);
+                                index = i;
+                            }
+                        }
+                        if ( index > -1 ) destination[d] = adjacents[index]["pano_id"];
+                        else destination[d] = null;
+                    }
                     curPos.lat = request["geometrie"]["coordinates"][1];
                     curPos.lng = request["geometrie"]["coordinates"][0];
+                    update_information_box();
+
                     var oldLogFunction = console.log;
                     console.log = function(){};//reset console.log
+
                     if ( first_draw ) { draw(); first_draw = false; }
-                    else{
+                    else{   // load panorama of the new location
                         var texture_small, texture_full;
                         new THREE.TextureLoader().load(imgurl_small, function (texture) {
                             texture_small = new THREE.MeshBasicMaterial( {
@@ -207,6 +268,7 @@ var AMSPano = {
                             //panorama.update_zoom();
                         });
                     }
+
                     console.log = oldLogFunction; // reset console.log
                 },
                 error:function(e){
@@ -283,15 +345,46 @@ var AMSPano = {
 
         var is_drag = false;
         var timmer_handle = null;
-        threeDview.onmousedown = function()
+        var start_x, start_y;
+        threeDview.onmousedown = function(ev)
         {
             is_drag = false;
             effectBlend.uniforms[ 'mixRatio' ].value = 0;
             threeDview.style.cursor = "move";
-            timmerHandle = setTimeout(function(){is_drag=true;},100); // interval(mousedown, mouseup)>100ms: drag
+            timmerHandle = setTimeout(function(){is_drag=true;},200); // interval(mousedown, mouseup)>200ms: drag
+            start_x = ev.clientX - panorama.offsetLeft - panorama.clientLeft;
+            start_y = ev.clientY - panorama.offsetTop - panorama.clientTop; // moving distance of mouse > 5: drag
         }
-        threeDview.onmousemove = function()
+        threeDview.onmousemove = function(ev)
         {
+            var mx = ev.clientX - panorama.offsetLeft - panorama.clientLeft;
+            var my = ev.clientY - panorama.offsetTop - panorama.clientTop;
+            mouse.x = (  mx/ panorama.clientWidth ) * 2 - 1;
+            mouse.y = - ( my / panorama.clientHeight ) * 2 + 1;
+            // update the picking ray with the camera and mouse position
+            if ( camera != null )
+            {
+                raycaster.setFromCamera( mouse, camera );
+                var intersects = raycaster.intersectObjects( scene.children );
+                for ( var i = 0; i < intersects.length; i++ )
+                    if ( intersects[i].object == navigator_plane )
+                    {
+                        navigator_pointer.position.x = intersects[i].point.x;
+                        navigator_pointer.position.z = intersects[i].point.z;
+
+                        //if accessible, change color
+                        var fov = 180/Math.pow(2,zoom);
+                        var bias = (mx - panorama.clientWidth/2)/panorama.clientWidth*fov; // if mouse doesn't click on the center
+                        var h = (180-(pov.heading-bias)+360)%360;
+                        if ( destination[parseInt(h/10)]!=null ) {
+                            navigator_pointer.material.color.set("green");
+                            navigator_pointer.geometry = navigator_geometry_yes;
+                        }else{
+                            navigator_pointer.material.color.set("red");
+                            navigator_pointer.geometry = navigator_geometry_no;
+                        }
+                    }
+            }
             if ( controls != null )
             {
                 pov = panorama.get_pov();
@@ -300,29 +393,22 @@ var AMSPano = {
         }
         threeDview.onmouseup = function(ev)
         {
+            var mx = ev.clientX - panorama.offsetLeft - panorama.clientLeft;
+            var my = ev.clientY - panorama.offsetTop - panorama.clientTop;
             threeDview.style.cursor = "pointer";
-            if ( is_drag ) { is_drag = false; return; }
+            if ( is_drag )
+                { is_drag = false; return; } // interval(mousedown, mouseup)>200ms
+            if ( (start_x-mx)*(start_x-mx)+(start_y-my)*(start_y-my)>25 )
+                { is_drag = false; return; } // moving distance of mouse > 5
             if ( controls == null ) return;
             var fov = 180/Math.pow(2,zoom);
-            var bias = (ev.clientX - panorama.clientWidth/2)/panorama.clientWidth*fov; // if mouse doesn't click on the center
+            var bias = (mx - panorama.clientWidth/2)/panorama.clientWidth*fov; // if mouse doesn't click on the center
             var h = (180-(pov.heading-bias)+360)%360;
-            var m = 0, index=-1;
-            // find adjacents according to json file
-            for ( var i = 0 ; i < adjacents.length ; i ++ )
+            if ( destination[parseInt(h/10)]!=null )    // prepare for changing panorama
             {
-                var dir = (heading+parseInt(adjacents[i]["direction"])+180)%360;
-                var dis = parseInt(adjacents[i]["distance"]);
-                if ( dis > 5 && Math.abs(dir-h) < 30 && m < parseInt(adjacents[i]["year"]) )
-                {
-                    m = parseInt(adjacents[i]["year"]);
-                    index = i;
-                }
-            }
-            if ( index > -1 )
-            {
-                transition_time = 1;
-                effectBlend.uniforms[ 'mixRatio' ].value = 0.95;
-                panorama.set(adjacents[index]["pano_id"]);
+                transition_time = 1;    // trigger transition animation
+                effectBlend.uniforms[ 'mixRatio' ].value = 0.95; // enable motion blur
+                panorama.set(destination[parseInt(h/10)]);
             }
         }
 
@@ -337,14 +423,34 @@ var AMSPano = {
             panorama.update_zoom();
         };
 
+        function update_information_box()
+        {
+            var info = "&nbsp;<b>Location:</b>&nbsp;("+curPos.lat+"N,"+curPos.lng+"E)&nbsp;";
+            info = info + "<br/>&nbsp;<b>Timestamp:</b>&nbsp;"+timestamp+"&nbsp;";
+            information_box.innerHTML = info;
+        }
+        information_panel.onclick = function()
+        {
+            if ( information_box.style.opacity == "0" ) information_box.style.opacity = "0.5";
+            else information_box.style.opacity = "0";
+            update_information_box();
+        }
+
         // CSS style
-        panorama.style.backgroundColor = "#646464";
-        panorama.style.overflow = "hidden";
-        panorama.style.color = "#fff";
-        panorama.style.textAlign = "center";
-        panorama.style.fontFamily = "Arial";
-        panorama.style.fontSize = "20px";
-        panorama.innerHTML="<br/><br/><br/><br/>loading panorama...";
+        panorama.innerHTML="<div style=\"position:absoulte;top:0%;left:0%;width:100%;height:100%\"></div>";
+
+        root.style.position = "absolute";
+        root.style.left = "0%";
+        root.style.top = "0%";
+        root.style.height = "100%";
+        root.style.width = "100%";
+        root.style.backgroundColor = "#646464";
+        root.style.overflow = "hidden";
+        root.style.color = "#fff";
+        root.style.textAlign = "center";
+        root.style.fontFamily = "Arial";
+        root.style.fontSize = "20px";
+        root.innerHTML="<br/><br/><br/><br/>loading panorama...";
 
         threeDview.style.position = "absolute";
         threeDview.style.left = "0%";
@@ -352,6 +458,33 @@ var AMSPano = {
         threeDview.style.height = "100%";
         threeDview.style.width = "100%";
         threeDview.style.cursor = "pointer";
+
+        information_panel.style.position = "absolute";
+        information_panel.style.top = "10px";
+        information_panel.style.left = "10px";
+        information_panel.style.width = "22px";
+        information_panel.style.height = "22px";
+        information_panel.style.background = "black";
+        information_panel.style.borderRadius = "11px";
+        information_panel.style.opacity = "0.5";
+        information_panel.style.zIndex = "10";
+        information_panel.style.textAlign = "center";
+        information_panel.style.fontFamily = "Times";
+        information_panel.style.fontSize = "18px";
+        information_panel.style.cursor = "pointer";
+        information_panel.innerHTML = "<i>i</i>";
+
+        information_box.style.position = "absolute";
+        information_box.style.top = "10px";
+        information_box.style.left = "40px";
+        information_box.style.height = "24px";
+        information_box.style.background = "black";
+        information_box.style.borderRadius = "5px";
+        information_box.style.opacity = "0";
+        information_box.style.zIndex = "10";
+        information_box.style.fontSize = "10px";
+        information_box.style.textAlign = "left";
+        information_box.innerHTML = "";
 
         zoominout.style.position = "absolute";
         zoominout.style.bottom = "20px";
@@ -410,8 +543,11 @@ var AMSPano = {
         compass_triangle_white.style.borderTop = "18px solid white";
         compass_triangle_white.style.opacity = "0.8";
 
-        panorama.appendChild(threeDview);
-        panorama.appendChild(zoominout);
+        panorama.appendChild(root);
+        root.appendChild(threeDview);
+        root.appendChild(information_panel);
+        root.appendChild(information_box);
+        root.appendChild(zoominout);
             zoominout.appendChild(zoomin);
             zoominout.appendChild(zoomout);
             zoominout.appendChild(compass);
